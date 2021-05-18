1fff2725bafd58edc69c99a7f94cd87cb81ca44f
NereaLopezFelipe <nerea.lfelipe@gmail.com>
2021-05-18 15:25:54 +0200

    sprites y librerias

--- ConsoleApplication1/Hombre.h
+++ ConsoleApplication1/Hombre.h
@@ -1,19 +1,37 @@
 #pragma once
+#include "ETSIDI.h"
 #include"Vector2D.h"
 
+using ETSIDI::SpriteSequence;
 
 class Hombre
 {
-public:
-	Hombre();
-	~Hombre();
-	void Dibuja();
-	void Mueve();
 
 private:
-	double altura;
+
+	SpriteSequence Andar; //sprite para caminar
+	SpriteSequence Parar; //sprite para cuando esta quieto
+	SpriteSequence Saltar; //sprite para saltos
+	SpriteSequence Morir; //sprite para la muerte del personaje
+
 	Vector2D posicion;
 	Vector2D velocidad;
 	Vector2D aceleracion;
+	int vida;
+	float altura;
+
+public:
+	Hombre();
+
+	void SetPosicion();
+	void SetVelocidad();
+	void SetAceleracion();
+	void SetVida(int _vida);
+	void SetAltura(float _altura);
+	~Hombre();
+
+	//funciones GLUT
+	void Dibuja();
+	void Mueve();
 };
 
